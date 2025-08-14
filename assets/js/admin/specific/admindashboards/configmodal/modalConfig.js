94725a38253fd55944510af3bde360bf378b9242
Brayden Tan <brayden.tan@pixl8.co.uk>
2025-08-14 15:38:35 +0800

    DASH-48 ensure modal config form validation run before saving

--- assets/js/admin/specific/admindashboards/configmodal/modalConfig.js
+++ assets/js/admin/specific/admindashboards/configmodal/modalConfig.js
@@ -1,5 +1,14 @@
 ( function( $ ){
-	window.getAdminDashboardWidgetConfig = function(){
+	window.getAdminDashboardWidgetConfig = function() {
 		return $( "#admin-dashboard-widget-config-form" ).serializeObject();
+	};
+
+	window.clearAllFormErrors = function() {
+		$( ".form-group .clearfix .help-block.error-message" ).remove();
+		$( ".form-group .has-error" ).removeClass( "has-error" );
+	};
+
+	window.getModalElements = function( fieldName ) {
+		return $( '[name="' + fieldName + '"]' );
 	};
 } )( presideJQuery );
